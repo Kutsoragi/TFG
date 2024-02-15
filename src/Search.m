f3775c0acdd79087fb1cacaa95cead3fea268db9
Fran <fferna07@ucm.es>
2024-02-15 17:32:47 +0100

    Busqueda finalizada, ahora muestra una pequeá información

--- src/Search.m
+++ src/Search.m
@@ -1,15 +1,14 @@
-function Search(red, netTransfer, Img, size)
+function [label, I] = Search(red, netTransfer, Img, size)
     filesPath = fullfile('..\ModelosEntrenados\', red);
     netTransfer = fullfile(filesPath, netTransfer);
     load(netTransfer);
     I = imread(Img);
     I = imresize(I, [size size]);
 
-    [YValidationPred,probs] = classify(netTransfer,I);
-z
-    figure
-    imshow(I)
+    [YValidationPred] = classify(netTransfer,I);
+
+
     label = YValidationPred(1);
-    title(string(label) + ", " + num2str(100*max(probs(1, :)),3) + "%");
+
     
 end
